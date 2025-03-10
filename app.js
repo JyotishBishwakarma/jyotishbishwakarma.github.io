c5e3068004674b9284f143d94242a13d08c516ba
Jyotish Bishwakarma <jyotish.bishwakarma@outlook.com>
2025-03-09 20:22:18 -0400

    jksdf

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('views', './views');
 app.use(express.static(path.join(__dirname, 'public')));
 // Middleware to receive email
 
-
+//just a comment
   // Example route
   app.get('/', (req, res) => {
     res.render('index');
